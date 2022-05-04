5bdd51cde579b6c3bb062960c55df337159ea463
Lucas Lucena <lucas.lucenak@gmail.com>
2022-05-04 14:13:30 -0300

    added sides rules tests

--- 5º Período/Métodos Avançados em Programação - Sabrina/Laboratório 01/src/test/java/example/TriangleTest.java
+++ 5º Período/Métodos Avançados em Programação - Sabrina/Laboratório 01/src/test/java/example/TriangleTest.java
@@ -6,9 +6,63 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest {
 
     @Test
-    public void ShouldReturnTrueWhenAllSidesAreZero() {
+    public void ShouldReturnNullWhenAllSidesAreZero() {
         TriangleException exception = Assertions.assertThrows(TriangleException.class, () -> {
             Triangle triangle = new Triangle(0, 0, 0);
+        });
+
+        Assertions.assertNull(exception.getMessage());
+    }
+
+    @Test
+    public void ShouldReturnNullWhenSide1IsZero() {
+        TriangleException exception = Assertions.assertThrows(TriangleException.class, () -> {
+            Triangle triangle = new Triangle(0, 5, 5);
+        });
+
+        Assertions.assertNull(exception.getMessage());
+    }
+
+    @Test
+    public void ShouldReturnNullWhenSide2IsZero() {
+        TriangleException exception = Assertions.assertThrows(TriangleException.class, () -> {
+            Triangle triangle = new Triangle(5, 0, 5);
+        });
+
+        Assertions.assertNull(exception.getMessage());
+    }
+
+    @Test
+    public void ShouldReturnNullWhenSide3IsZero() {
+        TriangleException exception = Assertions.assertThrows(TriangleException.class, () -> {
+            Triangle triangle = new Triangle(5, 5, 0);
+        });
+
+        Assertions.assertNull(exception.getMessage());
+    }
+
+    @Test
+    public void ShouldReturnNullWhenSide1And3AreZero() {
+        TriangleException exception = Assertions.assertThrows(TriangleException.class, () -> {
+            Triangle triangle = new Triangle(0, 5, 0);
+        });
+
+        Assertions.assertNull(exception.getMessage());
+    }
+
+    @Test
+    public void ShouldReturnNullWhenSide1And2AreZero() {
+        TriangleException exception = Assertions.assertThrows(TriangleException.class, () -> {
+            Triangle triangle = new Triangle(0, 0, 5);
+        });
+
+        Assertions.assertNull(exception.getMessage());
+    }
+
+    @Test
+    public void ShouldReturnNullWhenSide3And2AreZero() {
+        TriangleException exception = Assertions.assertThrows(TriangleException.class, () -> {
+            Triangle triangle = new Triangle(5, 0, 0);
         });
 
         Assertions.assertNull(exception.getMessage());
