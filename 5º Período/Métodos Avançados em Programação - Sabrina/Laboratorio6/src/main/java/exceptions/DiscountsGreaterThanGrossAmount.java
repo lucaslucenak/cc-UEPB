e19646f10e1a91a9163001ea1cc4c547f5202b38
Lucas Lucena <lucas.lucenak@gmail.com>
2022-06-22 15:54:33 -0300

    Add FinantialTest tests

--- 5º Período/Métodos Avançados em Programação - Sabrina/Laboratorio6/src/main/java/exceptions/DiscountsGreaterThanGrossAmount.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class DiscountsGreaterThanGrossAmount  extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public DiscountsGreaterThanGrossAmount(String msg) {
-        super(msg);
-    }
-}
