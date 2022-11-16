526c9b62d440c625702889f8749bad21e76774cb
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-15 22:23:51 -0300

    Update Paradigmas de Programação: Update project: Add comments

--- Paradigmas de Programação/Projeto (Linguagem R)/project/index.R
+++ Paradigmas de Programação/Projeto (Linguagem R)/project/index.R
@@ -1,9 +1,15 @@
 # Lê os arquivo e gera um dataframe "adresses"
 adresses<-read.table("data/adresses.txt",sep=",",row.names=1,quote="")
+
+# Gera vetores que vão armazenar os coneúdos das colunas especificadas pelo índice
 name <- adresses[,0]
 age <- adresses[,1]
 street <- adresses[,2]
 cep <- adresses[,3]
 
+# Gera um novo dataframe para inserir os vetores criados anteriormente
+# porém, com um cabeçalho agora
 df <- data.frame(name, age, street, cep)
+
+# Salva o arquivo criado no diretório específicado
 write.csv2(df, file='data/new-data.csv')
