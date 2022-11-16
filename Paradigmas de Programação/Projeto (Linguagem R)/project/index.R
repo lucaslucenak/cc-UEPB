d94cc4d22f0ef5d541cb99c7dc990b4a58fcbcb1
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-15 22:20:16 -0300

    Update Paradigmas: Add project

--- Paradigmas de Programação/Projeto (Linguagem R)/project/index.R
+++ Paradigmas de Programação/Projeto (Linguagem R)/project/index.R
@@ -1,8 +1,9 @@
-lower_number <- function(a, b) {
-  if (a < b) {
-    return (a)
-  } else {
-    return (b)
-  }
-}
-lower_number(5, 22)
+# Lê os arquivo e gera um dataframe "adresses"
+adresses<-read.table("data/adresses.txt",sep=",",row.names=1,quote="")
+name <- adresses[,0]
+age <- adresses[,1]
+street <- adresses[,2]
+cep <- adresses[,3]
+
+df <- data.frame(name, age, street, cep)
+write.csv2(df, file='data/new-data.csv')
