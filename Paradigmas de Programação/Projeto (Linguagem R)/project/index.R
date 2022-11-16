9f9aaab064040c37d4f7ab488422d8dd2def6170
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-15 22:37:20 -0300

    Update Paradigmas de Programação: Update project: Add order by age

--- Paradigmas de Programação/Projeto (Linguagem R)/project/index.R
+++ Paradigmas de Programação/Projeto (Linguagem R)/project/index.R
@@ -2,14 +2,17 @@
 adresses<-read.table("data/adresses.txt",sep=",",row.names=1,quote="")
 
 # Gera vetores que vão armazenar os coneúdos das colunas especificadas pelo índice
-name <- adresses[,0]
-age <- adresses[,1]
-street <- adresses[,2]
-cep <- adresses[,3]
+nomes <- adresses[,0]
+idades <- adresses[,1]
+ruas <- adresses[,2]
+ceps <- adresses[,3]
 
 # Gera um novo dataframe para inserir os vetores criados anteriormente
 # porém, com um cabeçalho agora
-df <- data.frame(name, age, street, cep)
+df <- data.frame(name=nomes, age=idades, street=ruas, cep=ceps)
+
+# Ordenando o dataframe pela idade
+df[order(df$age),]
 
 # Salva o arquivo criado no diretório específicado
 write.csv2(df, file='data/new-data.csv')
